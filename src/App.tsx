fc9b0338c0ac32ec0ae8a87974fcac49e7b8ff0a
YoussefWilliam <youssef.william997@gmail.com>
2022-05-17 15:31:49 +0200

    Adding json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Solution from "../types/Solution";
 
 function App() {
+  const [solution, setSolution] = useState<Solution>();
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((data) => {
+        const dataLength = data.length;
+        setSolution(data[Math.floor(Math.random() * dataLength)]);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wordle game</h1>
+      <div>Solution: {solution?.word}</div>
     </div>
   );
 }
