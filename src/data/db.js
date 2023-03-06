c6e8b4729f5487ee85782f45ee55e9c2b34c8694
YoussefWilliam <youssef.william997@gmail.com>
2023-03-06 20:58:02 +0200

    :zap: create random words

--- src/data/db.js
+++ src/data/db.js
@@ -1,3 +1,5 @@
+const randomWords = require("random-words");
+
 export const letters = [
   { key: "a" },
   { key: "b" },
@@ -26,20 +28,11 @@ export const letters = [
   { key: "y" },
   { key: "z" },
 ];
-export const solutions = [
-  { id: 1, word: "ninja" },
-  { id: 2, word: "spade" },
-  { id: 3, word: "pools" },
-  { id: 4, word: "drive" },
-  { id: 5, word: "relax" },
-  { id: 6, word: "times" },
-  { id: 7, word: "train" },
-  { id: 8, word: "cores" },
-  { id: 9, word: "pours" },
-  { id: 10, word: "blame" },
-  { id: 11, word: "banks" },
-  { id: 12, word: "phone" },
-  { id: 13, word: "bling" },
-  { id: 14, word: "coins" },
-  { id: 15, word: "hello" },
-];
+export const solutions = randomWords(100)
+  .filter((word) => word.length === 5)
+  .map((word, i) => {
+    return {
+      id: i,
+      word,
+    };
+  });
